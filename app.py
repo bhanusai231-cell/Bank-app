110244baba186b8c44edffc7ad6d96841865d274
Anjimeti bhanu sai <bhanusai231@gmail.com>
2026-07-09 17:24:56 +0530

    updated title

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ accounts = st.session_state.accounts
 # Title
 # ==========================================
 
-st.title("🏦 Smart Banking Management System")
+st.title("🏦 Bhanu Banking Management Pvt")
 
 st.write(
     "Manage customer accounts using a simple Streamlit application."
